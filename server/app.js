055c5f7caf38527fe3abbf75e240b4be2112b867
James-notebookComputer <907310116@qq.com>
2022-08-28 22:13:57 +0800

    安装express配置

---
+++ server/app.js
@@ -0,0 +1,9 @@
+// 导入express模块
+const express = require('express')
+// 创建服务器
+const server = express()
+
+// 调用app.listen方法
+app.listen(3008,()=>{
+  console.log('api server runing at http://127.0.0.1:3007')
+})
