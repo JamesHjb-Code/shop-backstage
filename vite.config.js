2442e9121e996a6f19ef1e3818039ee4488f66ea
James-notebookComputer <907310116@qq.com>
2022-09-01 21:54:14 +0800

    完成安装配置

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,13 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-
+import path from 'path'
 // https://vitejs.dev/config/
 export default defineConfig({
+  resolve:{
+    // 当使用文件系统路径的别名时，请始终使用绝对路径
+    alias:{
+      "~":path.resolve(__dirname,"src")
+    }
+  },
   plugins: [vue()]
 })
