94262c1c9dfb6643d749cf55c4fd4434e7be6de3
HuangJianbin <907310116@qq.com>
2022-09-19 17:31:58 +0800

    修改sql语句

--- server/router/login/sql.js
+++ server/router/login/sql.js
@@ -1,7 +1,7 @@
 // sql语句封装
 const loginSqls = {
-  login:'SELECT id,username,password FROM admin WHERE username=? and password=?',
-  getInfoByName:'SELECT id,username,password FROM admin WHERE username=?'
+  login:'SELECT id,username,password FROM adminInfo WHERE username=? and password=?',
+  getInfoByName:'SELECT id,username,password FROM adminInfo WHERE username=?'
 }
 // 导出sql语句
 module.exports = loginSqls
