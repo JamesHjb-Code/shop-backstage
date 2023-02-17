d15f32cb1d6dc6a5912a8d48ab148b1099af0870
HuangJianbin <907310116@qq.com>
2023-02-17 09:20:21 +0800

    移除sql文件

--- server/sql/admin_info.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO `admin_info`(`id`, `username`, `password`, `phone`, `address`, `avatar`, `roleLevel`) VALUES (1, 'admin', '123456', '18835234235', '广州市天河区', './src/assets/img/avator.jpg', 1);
-INSERT INTO `admin_info`(`id`, `username`, `password`, `phone`, `address`, `avatar`, `roleLevel`) VALUES (2, 'Huang', 'Huang', '16323342321', '深圳市南山区', './src/assets/img/avator.jpg', 2);
-INSERT INTO `admin_info`(`id`, `username`, `password`, `phone`, `address`, `avatar`, `roleLevel`) VALUES (6, 'wukekuan', '123456', '18819315903', '潮州市', './src/assets/img/avator.jpg', 2);
--- server/sql/slideimg.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO `slideimg`(`ID`, `img`, `url`, `goods_id`) VALUES (1, '/static/home/banner1.png', '/pages/goods_detail/main', 1);
-INSERT INTO `slideimg`(`ID`, `img`, `url`, `goods_id`) VALUES (2, '/static/home/banner2.png', '/pages/goods_detail/main', 2);
-INSERT INTO `slideimg`(`ID`, `img`, `url`, `goods_id`) VALUES (3, '/static/home/banner3.png', '/pages/goods_detail/main', 3);
