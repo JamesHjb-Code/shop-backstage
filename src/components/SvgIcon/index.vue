e49fa04657b01bb5e56113df51d2aa29d2baa299
HuangJianbin <907310116@qq.com>
2023-02-20 17:32:01 +0800

    封装svgIcon组件

---
+++ src/components/SvgIcon/index.vue
@@ -0,0 +1,11 @@
+<template>
+  <!-- 封装Icon -->
+  <svg class="svg-icon"
+       aria-hidden="true">
+    <use :xlink:href="iconName"></use>
+  </svg>
+</template>
+<script setup>
+</script>
+<style lang="scss" scoped>
+</style>
