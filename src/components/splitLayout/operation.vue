7769369e7b45f8674f624763e508f7a71b8175fa
HuangJianbin <907310116@qq.com>
2023-02-20 11:06:01 +0800

    根据监听是否全屏来判断图标变化

--- src/components/splitLayout/operation.vue
+++ src/components/splitLayout/operation.vue
@@ -1,25 +1,50 @@
 <template>
   <div class="operation-wrapper">
     <el-switch v-model="motifVal" />
-    <div class="screen-icon">
-      <!-- <img src="~/assets/full-screen.svg"> -->
+    <div class="screen-icon"
+         @click="changleFull()">
+      <img class="icon"
+           v-if="isFull"
+           src="../../assets/exit-screen.svg">
+      <img class="icon"
+           v-else
+           src="../../assets/full-screen.svg">
     </div>
   </div>
 </template>
 <script setup>
 import screenfull from 'screenfull'
-import { ref } from 'vue'
+import { ref, onMounted, onBeforeUnmount } from 'vue'
 const motifVal = ref(null)
 const isFull = ref(screenfull.isFullscreen)
-const changleFull = ()=>{
-  if(screenfull.isEnabled){
+onMounted(() => {
+  initSceen()
+})
+onBeforeUnmount(() => {
+  offSceen()
+})
+const changleFull = () => {
+  if (screenfull.isEnabled) {
     screenfull.toggle()
+  }
+}
+const changleIsFull = () => {
+  isFull.value = screenfull.isFullscreen
+}
+const initSceen = () => {
+  if (screenfull.isEnabled) {
+    screenfull.on('change', changleIsFull)
+  }
+}
+const offSceen = () => {
+  if (screenfull.isEnabled) {
+    screenfull.off('change', changleIsFull)
   }
 }
 </script>
 <style lang="scss" scoped>
 .operation-wrapper {
-  width:5vw;
+  width: 5vw;
   display: flex;
   align-content: center;
   align-items: center;
@@ -27,8 +52,12 @@ const changleFull = ()=>{
   padding: 10px;
   font-size: 20px;
   cursor: pointer;
-  .screen-icon{
-
+  .screen-icon {
+    display: flex;
+    .icon {
+      width: 25px;
+      height: 25px;
+    }
   }
 }
 </style>
