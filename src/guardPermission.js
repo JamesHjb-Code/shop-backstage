950c97d988e7b4ad83bd50c39b82404f24079d36
HuangJianbin <907310116@qq.com>
2023-02-28 17:27:55 +0800

    移除username

--- src/guardPermission.js
+++ src/guardPermission.js
@@ -3,17 +3,20 @@ import { getToken } from '~/compontool/token'
 import store from './store'
 // 全局前置守卫
 router.beforeEach(async (to, from, next) => {
+ 
   const token = getToken()
+
   // 没有token情况下，强制跳回登录页
   if (!token && to.path !== '/login') {
     return next({ path: "/login" }) //放行跳转
   }
+
   // 防止重复登录
   if (token && to.path === '/login') {
     return next({ path: from.path ? from.path : '/' })
   }
-  // 如果tohen且没有用户信息，自动获取管理员信息，并存储在vuex当中
-  if(token&&!store.state.userInfo.username){
+  // 如果tohen，自动获取管理员信息，并存储在vuex当中
+  if (token) {
     await store.dispatch("getInfo")
   }
   next()
