46c522786223b9766d5bcb2cd4f7b729cbc2f883
HuangJianbin <907310116@qq.com>
2023-03-06 17:36:13 +0800

    封装部分table组件

--- src/components/Table/index.vue
+++ src/components/Table/index.vue
@@ -1,5 +1,11 @@
 <template>
-
+<!-- 封装Table表格组件 -->
+  <el-table>
+    <!-- 是否显示全选 -->
+    <template v-if="tableConfig.isSelect"></template>
+    <!-- 是否显示序号 -->
+    <template v-if="tableConfig.isIndex"></template>
+  </el-table>
 </template>
 
 <script setup name="table">
