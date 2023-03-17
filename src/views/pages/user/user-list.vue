27f8604dac1c604fcd088ea021a1a1e547562ba9
HuangJianbin <907310116@qq.com>
2023-03-17 17:38:21 +0800

    引入table组件

---
+++ src/views/pages/user/user-list.vue
@@ -0,0 +1,12 @@
+<template>
+  <table> </table>
+</template>
+<script setup name="userList">
+import Table from '~/components/Table/index.vue'
+
+
+</script>
+
+
+<style lang="scss" scoped>
+</style>
